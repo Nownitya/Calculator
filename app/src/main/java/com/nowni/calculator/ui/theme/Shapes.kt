2246199e929f950e604a76775fc2c29c0e79089b
Nownitya Sharma <nownitya@gmail.com>
2025-05-19 11:24:59 +0530

    Calculator_V3.

--- app/src/main/java/com/nowni/calculator/ui/theme/Shapes.kt
+++ app/src/main/java/com/nowni/calculator/ui/theme/Shapes.kt
@@ -12,14 +12,3 @@ val Shapes: Shapes = Shapes(
     extraLarge = RoundedCornerShape(24.dp)
 )
 
-
-
-object NeumorphicShapes{
-    val Button = RoundedCornerShape(16.dp)
-    // Display area with inset look
-    val Display = RoundedCornerShape(12.dp)
-    // Pressed state shape (slightly smaller radius)
-    val Pressed = RoundedCornerShape(14.dp)
-    // Extended width buttons (0 and =)
-    val WideButton = RoundedCornerShape(16.dp)
-}
